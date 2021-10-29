89ffdbfd91d9ea6c0ef6d19d0956b08c280e5abc
otter9459 <otter9459@gmail.com>
2021-10-30 01:09:53 +0900

    Add: Add environment variable for AWS parameter store

--- server/scripts/start.sh
+++ server/scripts/start.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd /home/ubuntu/TheKing-sLetters/server
 
+export DATABASE_USER=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_USER --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_PASSWORD=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_PASSWORD --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_HOST=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_HOST --query Parameters[0].Value | sed 's/"//g')
+export DATABASE_NAME=$(aws ssm get-parameters --region ap-northeast-2 --names DATABASE_NAME --query Parameters[0].Value | sed 's/"//g')
+export NODEMAILER_USER=$(aws ssm get-parameters --region ap-northeast-2 --names NODEMAILER_USER --query Parameters[0].Value | sed 's/"//g')
+export NODEMAILER_PASS=$(aws ssm get-parameters --region ap-northeast-2 --names NODEMAILER_PASS --query Parameters[0].Value | sed 's/"//g')
+export ACCESS_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ACCESS_SECRET --query Parameters[0].Value | sed 's/"//g')
+export ADMIN_SECRET=$(aws ssm get-parameters --region ap-northeast-2 --names ADMIN_SECRET --query Parameters[0].Value | sed 's/"//g')
+
 authbind --deep pm2 start index.js

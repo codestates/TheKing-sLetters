e5975bfd5347738b75b5f5f75de1d6beebf56550
improvise0828 <jnkbs828@gmail.com>
2021-11-05 17:06:40 +0900

    Deleted: react-redux; Added: react context;

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,11 @@ import LandingPage from './Views/Pages/Landing/LandingPage';
 import Mypage from './Views/Pages/MyPage/MyPage';
 import Main from './Views/Pages/Main/Main';
 import QuizPost from './Views/Pages/QuizPost/QuizPost';
+import QuizSolve from './Views/Pages/QuizSolve/QuizSolve';
 import Admin from './Views/Pages/Admin/AdminPage';
 import ProblemBox from './Views/Pages/ProblemBox/ProblemBox';
 
 function App() {
-  const [AdminLogin, setAdminLogin] = useState([]);
-
   return (
     <>
       <Header />
@@ -24,6 +23,7 @@ function App() {
         <Route exact path="/quizpost" component={QuizPost}></Route>
         <Route exact path="/admin" component={Admin}></Route>
         <Route exact path="/mynote" component={ProblemBox}></Route>
+        <Route path="/quizsolve/:id" component={QuizSolve}></Route>
       </Switch>
       <Footer />
     </>
